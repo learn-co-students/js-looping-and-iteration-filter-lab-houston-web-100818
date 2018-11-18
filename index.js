744d594de5c68042365e99aadf0674ee3651e875
Bilikis Orulebaja <bilkisbalogun@gmail.com>
2018-11-18 16:48:01 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution in this file
+function findMatching(drivers, string) {
+  const result = drivers.filter(function(str) {
+    return str.toUpperCase() === string.toUpperCase();
+  });
+  return result;
+}
+
+function fuzzyMatch(drivers, string) {
+  const result = drivers.filter(function(str) {
+    return str.slice(0, string.length) === string;
+  });
+  return result;
+}
+
+function matchName(drivers, string) {
+  const result = drivers.filter(function(str) {
+    return str.name === string;
+  });
+  return result;
+}
