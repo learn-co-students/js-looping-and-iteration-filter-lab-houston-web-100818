b058b18369791eae65373fc79617412d8c529556
no-relation <eddiec76@gmail.com>
2018-11-19 11:58:38 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution in this file
+function findMatching(array, name){
+    return array.filter(function(element){
+        return element.toLowerCase() === name.toLowerCase()
+    })
+};
+
+function fuzzyMatch(array, substring){
+    return array.filter(
+        function (element){
+            return element.slice(0,substring.length) === substring
+        }
+    )
+};
+
+function matchName(array, name){
+    return array.filter(
+        function (element){
+            return element.name.toLowerCase() === name.toLowerCase()
+        }
+    )
+};
